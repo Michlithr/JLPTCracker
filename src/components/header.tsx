13680ea58b7551cba63438ad054e2adb8abc4505
Michal Litwa <michal.litwa@qiagen.com>
2024-06-04 15:56:13 +0200

    Commit with main page layout and header implementation

---
+++ src/components/header.tsx
@@ -0,0 +1,23 @@
+import styles from "./header.module.scss";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <span className={`${styles.homeIcon} material-symbols-outlined`}>
+        home
+      </span>
+      <div className={styles.navLinksContainer}>
+        <Link href="/kanji" className={styles.navLink}>
+          Kanji
+        </Link>
+        <Link href="/vocabulary" className={styles.navLink}>
+          Vocabulary
+        </Link>
+        <Link href="/grammar" className={styles.navLink}>
+          Grammar
+        </Link>
+      </div>
+    </header>
+  );
+}
